f0dbf2d47210c1940b27e589329825b17064e9c7
Kristopher Elers <kristopherelers@gmail.com>
2022-11-17 02:25:12 -0600

    Added text feedback

--- update.py
+++ update.py
@@ -9,8 +9,9 @@ import glob
 import os
 import shutil
 
-SOURCE_DIR = '~/.dotfiles/dots'
-IGNORE = ['.DS_Store']
+SOURCE_DIR = "~/.dotfiles/dots"
+IGNORE = [".DS_Store"]
+
 
 def force_remove(path):
     if os.path.isdir(path) and not os.path.islink(path):
@@ -18,32 +19,36 @@ def force_remove(path):
     else:
         os.unlink(path)
 
+
 def is_link_to(link, dest):
     is_link = os.path.islink(link)
-    is_link = is_link and os.readlink(link).rstrip('/') == dest.rstrip('/')
+    is_link = is_link and os.readlink(link).rstrip("/") == dest.rstrip("/")
     return is_link
 
+
 def main():
-    print(f'Starting syncronization at {SOURCE_DIR}...')
+    print(f"Starting syncronization at {SOURCE_DIR}...")
     os.chdir(os.path.expanduser(SOURCE_DIR))
-    for filename in [file for file in glob.glob('.*') if file not in IGNORE]:
-        dotfile = os.path.join(os.path.expanduser('~'), filename)
-        source = os.path.join(SOURCE_DIR, filename).replace('~', '.')
+    for filename in [file for file in glob.glob(".*") if file not in IGNORE]:
+        dotfile = os.path.join(os.path.expanduser("~"), filename)
+        source = os.path.join(SOURCE_DIR, filename).replace("~", ".")
 
         # Check that we aren't overwriting anything
         if os.path.lexists(dotfile):
             if is_link_to(dotfile, source):
                 continue
 
-            response = input(f'Overwrite file \'{dotfile}\'? [y/N] ')
-            if not response.lower().startswith('y'):
+            response = input(f"Overwrite file '{dotfile}'? [y/N] ")
+            if not response.lower().startswith("y"):
                 continue
-            
-            print(f'Creating symlink to {dotfile} in home directory')
+
+            print(f"Creating symlink to {dotfile} in home directory")
             force_remove(dotfile)
 
         os.symlink(source, dotfile)
-        print(f'{dotfile} => {source}')
+        print(f"{dotfile} => {source}")
+    print("Process completed!")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
