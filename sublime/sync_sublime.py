4e2343aeca77079aa5e953823290ecc7c88aec4a
Kristopher Elers <kristopherelers@gmail.com>
2022-11-18 03:12:56 -0600

    Finished sync_sublime.py

--- sublime/sync_sublime.py
+++ sublime/sync_sublime.py
@@ -7,9 +7,10 @@ Sublime Text syncronization. Useful for updating changes made to Sublime remotel
 import glob
 import os
 import shutil
+import sys
 
 SOURCE_DIR = "~/.dotfiles/sublime"
-DESTINATION_DIR = "~/Library/Application\ Support/Sublime\ Text/Packages/User"
+DESTINATION_DIR = "~/Library/Application Support/Sublime Text/Packages/User"
 IGNORE = [
     ".DS_Store",
     "readme.md",
@@ -19,44 +20,25 @@ IGNORE = [
 ]
 
 
-def force_remove(path):
-    if os.path.isdir(path) and not os.path.islink(path):
-        shutil.rmtree(path, False)
-    else:
-        os.unlink(path)
-
-
-def is_link_to(link, dest):
-    is_link = os.path.islink(link)
-    is_link = is_link and os.readlink(link).rstrip("/") == dest.rstrip("/")
-    return is_link
-
-
 def main():
     print(f"Starting syncronization at {SOURCE_DIR}...\n")
     os.chdir(os.path.expanduser(SOURCE_DIR))
-    for filename in [file for file in glob.glob(".sublime*") if file not in IGNORE]:
-        current_file = os.path.join(os.path.expanduser("~"), filename)
-        print(current_file)
-        source = os.path.join(SOURCE_DIR, filename).replace("~", ".")
-        print(source)
+    for filename in [file for file in glob.glob("*") if file not in IGNORE]:
+        source = os.path.join(os.path.expanduser(SOURCE_DIR), filename)
+        destination = os.path.join(os.path.expanduser(DESTINATION_DIR), filename)
 
-        # Check that we aren't overwriting anything
-        if os.path.lexists(current_file):
-            if is_link_to(current_file, source):
-                print(f"Symlink to {source} already exists in {current_file}")
-                continue
+        # Check that we aren't automatically overwriting anything
+        if os.path.exists(destination):
+            response = input(f"Overwrite current '{filename}'? [y/N] ")
 
-            response = input(f"Overwrite file '{current_file}'? [y/N] ")
             if not response.lower().startswith("y"):
                 continue
 
-            print(f"Creating symlink to {current_file} in home directory")
-            # force_remove(current_file)
-
-        # os.symlink(source, current_file)
-        print(f"{current_file} => {source}")
-    print("\nProcess completed!")
+        try:
+            shutil.copy(source, destination)
+        except IOError:
+            print(f"Error copying {filename}. Destination not writable.")
+            sys.exit()
 
 
 if __name__ == "__main__":
